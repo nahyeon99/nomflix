1e23af21942cc21036af9f324bb7b34392476c20
NaHyeon <nahyeonee99@gmail.com>
2021-05-13 15:33:30 +0900

    #3.2 CSS in React part Three

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
-import Header from "Components/Header/Header";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
         <Router />
       </>
     );
---
+++ src/Components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+// const Item = styled.li``;
+
+const SLink = styled(Link)``;
+
+export default () => (
+  <header>
+    <List>
+      <li>
+        <SLink to="/">Movies</SLink>
+      </li>
+      <li>
+        <SLink to="/tv">TV</SLink>
+      </li>
+      <li>
+        <SLink to="/search">Search</SLink>
+      </li>
+    </List>
+  </header>
+);
