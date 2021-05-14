93ebb5720940a47d636c3ecc64d7a7960e75d266
NaHyeon <nahyeonee99@gmail.com>
2021-05-15 01:21:33 +0900

    #5.5

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,27 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default class extends React.Component {
   state = {
     result: null,
     loading: true,
     error: null,
   };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+  }
 
   render() {
     const { result, loading, error } = this.state;
