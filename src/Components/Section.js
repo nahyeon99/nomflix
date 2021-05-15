21887c18db9f24c1f6cd28a6c2b0ca3b34a1524d
NaHyeon <nahyeonee99@gmail.com>
2021-05-15 13:25:36 +0900

    #6.1

---
+++ src/Components/Section.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import propTypes from "prop-types";
+
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 50px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Grid = styled.div`
+  margin-top: 25px;
+`;
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </Container>
+);
+
+Section.propTypes = {
+  title: propTypes.string.isRequired,
+  children: propTypes.oneOfType([
+    propTypes.arrayOf(propTypes.node),
+    propTypes.node,
+  ]),
+};
+
+export default Section;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,16 +1,39 @@
 import React from "react";
-import PropTypes from "prop-types";
+import Section from "Components/Section";
 import styled from "styled-components";
+import propTypes from "prop-types";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map((movie) => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
-HomePresenter.PropTypes = {
-  nowPlaying: PropTypes.array,
-  upcoming: PropTypes.array,
-  popular: PropTypes.array,
-  loading: PropTypes.bool.isRequired,
-  error: PropTypes.string,
+HomePresenter.propTypes = {
+  nowPlaying: propTypes.array,
+  upcoming: propTypes.array,
+  popular: propTypes.array,
+  loading: propTypes.bool.isRequired,
+  error: propTypes.string,
 };
 
 export default HomePresenter;
